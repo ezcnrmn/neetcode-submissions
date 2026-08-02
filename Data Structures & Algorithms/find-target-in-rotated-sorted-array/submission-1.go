7df8899536ba836d516bbfa3c93447272ea62404
ezcnrmn <47943395+ezcnrmn@users.noreply.github.com>
2026-08-02 15:37:05 +0700

    Add: find-target-in-rotated-sorted-array - submission-1

---
+++ Data Structures & Algorithms/find-target-in-rotated-sorted-array/submission-1.go
@@ -0,0 +1,25 @@
+func search(nums []int, target int) int {
+	var left int
+	right := len(nums)-1
+	for left <= right {
+		mid := left + (right-left)/2
+		if nums[mid] == target {
+			return mid
+		}
+
+		if nums[mid] < nums[right] {
+			if target < nums[mid] || target >= nums[right] {
+				right = mid-1
+			} else {
+				left = mid+1
+			}
+		} else {
+			if target > nums[mid] || target <= nums[right] {
+				left = mid+1
+			} else {
+				right = mid-1
+			}
+		}
+	}
+	return -1
+}
