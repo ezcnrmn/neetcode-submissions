2733126a3a848219056739d0a1324f3ce4fed583
ezcnrmn <47943395+ezcnrmn@users.noreply.github.com>
2026-08-02 16:42:02 +0700

    Add: reorder-linked-list - submission-2

---
+++ Data Structures & Algorithms/reorder-linked-list/submission-2.go
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+
+func reorderList(head *ListNode) {
+	fast, slow := head, head
+	for fast.Next != nil && fast.Next.Next != nil {
+		slow = slow.Next
+		fast = fast.Next.Next
+	}
+
+	cur := slow.Next
+	var prev *ListNode
+	for cur != nil{
+		cur.Next, prev, cur = prev, cur, cur.Next
+	}
+
+	p1, p2 := head, prev
+	for p2 != nil {
+		p1, p2, p1.Next, p2.Next = p1.Next, p2.Next, p2, p1.Next
+	}
+	if p1 != nil {
+		p1.Next = nil
+	}
+
+	return 
+}
