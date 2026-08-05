5f6a68a8e93ffab450bdd9442c50ef8fb9ea7a6a
ezcnrmn <47943395+ezcnrmn@users.noreply.github.com>
2026-08-05 21:29:08 +0700

    Add: time-based-key-value-store - submission-2

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-2.go
@@ -0,0 +1,63 @@
+type timeMark struct {
+	timestamp int
+	value     string
+}
+
+type TimeMap struct {
+	m map[string][]timeMark
+}
+
+func Constructor() TimeMap {
+	tm := TimeMap{
+		m: make(map[string][]timeMark),
+	}
+	return tm
+}
+
+func (this *TimeMap) Set(key string, value string, timestamp int) {
+	this.m[key] = append(this.m[key], timeMark{timestamp: timestamp, value: value})
+	/*
+	// for not increasing timestamps :D
+	index := this.binarySearch(this.m[key], timestamp)
+	if index != -1 && this.m[key][index].timestamp == timestamp {
+		this.m[key][index].value = value
+		return
+	}
+
+	this.m[key] = append(this.m[key], timeMark{})
+	for i := len(this.m[key])-1; i > index+1; i-- {
+		this.m[key][i] = this.m[key][i-1]
+	}
+	this.m[key][index+1] = timeMark{timestamp: timestamp, value: value}
+	*/
+}
+
+func (this *TimeMap) Get(key string, timestamp int) string {
+	index := this.binarySearch(this.m[key], timestamp)
+	if index == -1 {
+		return ""
+	}
+
+	return this.m[key][index].value
+}
+
+func (this *TimeMap) binarySearch(marks []timeMark, timestamp int) int {
+	left, right := 0, len(marks)-1
+	if len(marks) > 0 && marks[0].timestamp > timestamp {
+		return -1
+	}
+	for left <= right {
+		mid := left + (right-left)/2
+		if mid == len(marks)-1 || (marks[mid].timestamp <= timestamp && marks[mid+1].timestamp > timestamp) {
+			return mid
+		}
+
+		if marks[mid].timestamp > timestamp {
+			right = mid-1
+		} else {
+			left = mid+1
+		}
+	}
+
+	return -1
+}
