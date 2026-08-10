4448a29196d7636f4df0047a1e21a96bf423aac4
ezcnrmn <47943395+ezcnrmn@users.noreply.github.com>
2026-08-10 17:39:27 +0700

    Add: count-good-nodes-in-binary-tree - submission-1

---
+++ Data Structures & Algorithms/count-good-nodes-in-binary-tree/submission-1.go
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+type StackItem struct {
+	Node *TreeNode
+	Max  int
+}
+
+func goodNodes(root *TreeNode) int {
+	stack := []StackItem{{Node: root, Max: -100}}
+	var result int
+	for len(stack) > 0 {
+		cur := stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+		if cur.Node == nil {
+			continue
+		}
+		if cur.Node.Val >= cur.Max {
+			result++
+		}
+		curMax := max(cur.Max, cur.Node.Val)
+		stack = append(stack, StackItem{Node: cur.Node.Right, Max: curMax})
+		stack = append(stack, StackItem{Node: cur.Node.Left, Max: curMax})
+	}
+	return result
+}
