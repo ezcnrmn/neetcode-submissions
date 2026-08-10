fa7fd1db7deae9037b29d3a2549c366b53f58a0f
ezcnrmn <47943395+ezcnrmn@users.noreply.github.com>
2026-08-10 16:38:47 +0700

    Add: lowest-common-ancestor-in-binary-search-tree - submission-1

---
+++ Data Structures & Algorithms/lowest-common-ancestor-in-binary-search-tree/submission-1.go
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
+	stack := []*TreeNode{root}
+	for len(stack) > 0 {
+		cur := stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+		switch {
+		case cur.Val == p.Val || cur.Val == q.Val,
+			(cur.Val > p.Val && cur.Val < q.Val) || (cur.Val < p.Val && cur.Val > q.Val):  
+			return cur
+		case cur.Val > p.Val && cur.Val > q.Val:
+			stack = append(stack, cur.Left)
+		default:
+			stack = append(stack, cur.Right)
+		}
+	}
+	return nil
+}
