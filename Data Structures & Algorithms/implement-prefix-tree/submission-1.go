c075492376be2948ff25e5400510fc3668a6e773
ezcnrmn <47943395+ezcnrmn@users.noreply.github.com>
2026-08-13 16:26:34 +0700

    Add: implement-prefix-tree - submission-1

---
+++ Data Structures & Algorithms/implement-prefix-tree/submission-1.go
@@ -0,0 +1,45 @@
+type Node struct {
+	Children [26]*Node
+	IsFinal  bool
+}
+
+type PrefixTree struct {
+	Head *Node
+}
+
+func Constructor() PrefixTree {
+	return PrefixTree{Head: &Node{IsFinal: true}}
+}
+
+func (this *PrefixTree) Insert(word string) {
+	cur := this.Head
+	for _, char := range word {
+		if cur.Children[char-'a'] == nil {
+			cur.Children[char-'a'] = &Node{}
+		}
+		cur = cur.Children[char-'a']
+	}
+	cur.IsFinal = true
+}
+
+func (this *PrefixTree) Search(word string) bool {
+	cur := this.Head
+	for _, char := range word {
+		if cur.Children[char-'a'] == nil {
+			return false
+		}
+		cur = cur.Children[char-'a']
+	}
+	return cur.IsFinal
+}
+
+func (this *PrefixTree) StartsWith(prefix string) bool {
+	cur := this.Head
+	for _, char := range prefix {
+		if cur.Children[char-'a'] == nil {
+			return false
+		}
+		cur = cur.Children[char-'a']
+	}
+	return true
+}
