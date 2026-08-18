4b988d62785d77ca7529f798db2cfa368a3f092f
ezcnrmn <47943395+ezcnrmn@users.noreply.github.com>
2026-08-18 20:32:15 +0700

    Add: meeting-schedule - submission-1

---
+++ Data Structures & Algorithms/meeting-schedule/submission-1.go
@@ -0,0 +1,22 @@
+/**
+ * Definition of Interval:
+ * type Interval struct {
+ *    start int
+ *    end   int
+ * }
+ */
+
+import "slices"
+
+func canAttendMeetings(intervals []Interval) bool {
+	slices.SortFunc(intervals, func(a, b Interval) int {
+		return a.start - b.start
+	})
+	for i := 1; i < len(intervals); i++ {
+		cur, prev := intervals[i], intervals[i-1]
+		if cur.start < prev.end {
+			return false
+		}
+	}
+	return true
+}
