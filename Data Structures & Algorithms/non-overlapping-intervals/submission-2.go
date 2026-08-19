48fd5667f1072865d718547ced43c83b7223029a
ezcnrmn <47943395+ezcnrmn@users.noreply.github.com>
2026-08-19 18:40:39 +0700

    Add: non-overlapping-intervals - submission-2

---
+++ Data Structures & Algorithms/non-overlapping-intervals/submission-2.go
@@ -0,0 +1,18 @@
+import "slices"
+
+func eraseOverlapIntervals(intervals [][]int) int {
+	slices.SortFunc(intervals, func(a, b []int) int {
+		return a[0] - b[0]
+	})
+	lastEnd := intervals[0][1]
+	var result int
+	for i := 1; i < len(intervals); i++ {
+		if lastEnd <= intervals[i][0] {
+			lastEnd = intervals[i][1]
+		} else {
+			result++
+			lastEnd = min(lastEnd, intervals[i][1])
+		}
+	}
+	return result
+}
