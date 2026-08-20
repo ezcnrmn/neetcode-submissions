56bf9ae007a30f1a1d30fe4e622670b9ce28ea2c
ezcnrmn <47943395+ezcnrmn@users.noreply.github.com>
2026-08-20 19:25:28 +0700

    Add: clone-graph - submission-3

---
+++ Data Structures & Algorithms/clone-graph/submission-3.go
@@ -0,0 +1,35 @@
+/**
+ * Definition for a Node.
+ * type Node struct {
+ *     Val int
+ *     Neighbors []*Node
+ * }
+ */
+
+func cloneGraph(node *Node) *Node {
+	if node == nil {
+		return node
+	}
+
+	queue := []*Node{node}
+	seen := make(map[*Node]*Node)
+	seen[node] = &Node{Val: node.Val}
+	for len(queue) > 0 {
+		cur := queue[0]
+		queue[0] = nil
+		queue = queue[1:]
+		clone := seen[cur]
+
+		for _, n := range cur.Neighbors {
+			cloneN, ok := seen[n]
+			if !ok {
+				cloneN = &Node{Val: n.Val}
+				seen[n] = cloneN
+				queue = append(queue, n)
+			}
+			clone.Neighbors = append(clone.Neighbors, cloneN)
+		}
+	}
+	return seen[node]
+}
+
