259bfbce51cc41c29e8b4755f43b913fb41bf930
ezcnrmn <47943395+ezcnrmn@users.noreply.github.com>
2026-08-24 18:18:55 +0700

    Add: islands-and-treasure - submission-0

---
+++ Data Structures & Algorithms/islands-and-treasure/submission-0.go
@@ -0,0 +1,28 @@
+func islandsAndTreasure(grid [][]int) {
+    for row := range grid {
+        for col := range grid[0] {
+            if grid[row][col] == 0 {
+                fillDistances(grid, row, col)
+            }
+        }
+    }
+}
+
+func fillDistances(grid [][]int, row, col int) {
+    queue := [][3]int{{row, col, 0}}
+    for len(queue) > 0 {
+        cur := queue[0]
+        queue = queue[1:]
+        for _, d := range [...][2]int{{1, 0}, {-1, 0}, {0, 1}, {0, -1}} {
+            newRow, newCol, distance := cur[0] + d[0], cur[1] + d[1], cur[2] + 1
+            if newRow < 0 || newRow >= len(grid) || newCol < 0 || newCol >= len(grid[0]) {
+                continue
+            }
+
+            if grid[newRow][newCol] > distance {
+                grid[newRow][newCol] = distance
+                queue = append(queue, [3]int{newRow, newCol, distance})
+            }
+        }
+    }
+}
