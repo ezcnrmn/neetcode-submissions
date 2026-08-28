c38cf445b27330099beae88208f60fc94f0d1a31
ezcnrmn <47943395+ezcnrmn@users.noreply.github.com>
2026-08-28 16:58:23 +0700

    Add: course-schedule-ii - submission-0

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-0.go
@@ -0,0 +1,38 @@
+func findOrder(numCourses int, prerequisites [][]int) []int {
+	indegree := make(map[int]int, numCourses)
+	for course := range numCourses {
+		indegree[course] = 0
+	}
+
+	graph := make(map[int][]int, numCourses)
+	for _, p := range prerequisites {
+		graph[p[1]] = append(graph[p[1]], p[0])
+		indegree[p[0]]++
+	}
+
+	var queue []int
+	for course, amount := range indegree {
+		if amount == 0 {
+			queue = append(queue, course)
+		}
+	}
+
+	result := make([]int, 0, numCourses)
+	for len(queue) > 0 {
+		course := queue[0]
+		queue = queue[1:]
+		result = append(result, course)
+		for _, neighbor := range graph[course] {
+			indegree[neighbor]--
+			if indegree[neighbor] == 0 {
+				queue = append(queue, neighbor)
+			}
+		}
+	}
+
+	if len(result) != numCourses {
+		return []int{}
+	}
+
+	return result
+}
