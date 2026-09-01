3977baf01266cf4d757d4b8680d836e61617ed06
ezcnrmn <47943395+ezcnrmn@users.noreply.github.com>
2026-09-01 21:36:36 +0700

    Add: redundant-connection - submission-1

---
+++ Data Structures & Algorithms/redundant-connection/submission-1.go
@@ -0,0 +1,47 @@
+func findRedundantConnection(edges [][]int) []int {
+    n := len(edges)
+
+    graph := make(map[int][]int, n)
+    indexes := make(map[[2]int]int, n*2)
+    indegree := make(map[int]int, n)
+    for i, edge := range edges {
+        graph[edge[0]] = append(graph[edge[0]], edge[1])
+        graph[edge[1]] = append(graph[edge[1]], edge[0])
+        indexes[[2]int{edge[0], edge[1]}] = i
+        indexes[[2]int{edge[1], edge[0]}] = i
+        indegree[edge[0]]++
+        indegree[edge[1]]++
+    }
+
+    queue := make([]int, 0, n)
+    for node, i := range indegree {
+        if i == 1 {
+            queue = append(queue, node)
+        }
+    }
+
+    for len(queue) > 0 {
+        node := queue[0]
+        queue = queue[1:]
+        for _, ngh := range graph[node] {
+            indegree[ngh]--
+            if indegree[ngh] == 1 {
+                queue = append(queue, ngh)
+            }
+        }
+    }
+
+    var resultIndex int
+    for node, nodeI := range indegree {
+        if nodeI != 2 {
+            continue
+        }
+        for _, ngh := range graph[node] {
+            if nghI, ok := indegree[ngh]; ok && nghI == 2 {
+                resultIndex = max(resultIndex, indexes[[2]int{node, ngh}])
+            }
+        }
+    }
+
+    return edges[resultIndex]
+}
