b851b16379d18d36612a590e9d084182c2275a63
ezcnrmn <47943395+ezcnrmn@users.noreply.github.com>
2026-09-22 21:26:23 +0700

    Add: cheapest-flight-path - submission-1

---
+++ Data Structures & Algorithms/cheapest-flight-path/submission-1.go
@@ -0,0 +1,80 @@
+func findCheapestPrice(n int, flights [][]int, src int, dst int, k int) int {
+    graph := make(map[int][][2]int, n)
+    for _, flight := range flights {
+        graph[flight[0]] = append(graph[flight[0]], [2]int{flight[1], flight[2]})
+    }
+
+    minHeap := &MinHeap{{Num: src}}
+    var result int
+
+    for minHeap.Len() > 0{
+        cur := heap.Pop(minHeap).(HeapItem)
+        
+        for _, flight := range graph[cur.Num] {
+            flightDst, flightPrice := flight[0], flight[1]
+
+            if dst == flightDst {
+                /*
+                if result == 0 || result > cur.Price + flightPrice {
+                    result = cur.Price + flightPrice
+                }
+                continue
+                */
+                return cur.Price + flightPrice
+            }
+
+            if cur.Jumps + 1 > k  {
+                continue
+            }
+
+            heap.Push(minHeap, HeapItem{Num: flightDst, Price: cur.Price + flightPrice, Jumps: cur.Jumps+1})
+        }
+    }
+
+    if result == 0 {
+        return -1
+    }
+    return result
+}
+
+/*
+h := &MinHeap{2, 1, 5}
+heap.Init(h)
+heap.Push(h, 3)
+fmt.Printf("minimum: %d\n", (*h)[0])
+for h.Len() > 0 {
+    fmt.Printf("%d ", heap.Pop(h))
+}
+*/
+
+
+type HeapItem struct {
+    Num   int
+    Price int
+    Jumps int
+}
+type MinHeap [] HeapItem
+
+func (h MinHeap) Len() int {
+    return len(h)
+}
+
+func (h MinHeap) Less(i, j int) bool {
+    return h[i].Price < h[j].Price
+}
+
+func (h MinHeap) Swap(i, j int) {
+    h[i], h[j] = h[j], h[i]
+}
+
+func (h *MinHeap) Push(x any) {
+	*h = append(*h, x.(HeapItem))
+}
+
+func (h *MinHeap) Pop() any {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[0 : n-1]
+	return x
+}
